1729e111f7e67b24011974c95425ae820bf4b63a
Maxime Kohlhaas <mko@atm-consulting.fr>
2013-10-28 16:09:57 +0100

    Ajout liste spécifique pour Latoxan

--- core/modules/moddispatch.class.php
+++ core/modules/moddispatch.class.php
@@ -218,6 +218,19 @@ class moddispatch extends DolibarrModules
 		//							'target'=>'',
 		//							'user'=>2);				                // 0=Menu for internal users, 1=external users, 2=both
 		// $r++;
+		$this->menu[$r]=array(	'fk_menu'=>'fk_mainmenu=products,fk_leftmenu=sendings',		    // Use 'fk_mainmenu=xxx' or 'fk_mainmenu=xxx,fk_leftmenu=yyy' where xxx is mainmenucode and yyy is a leftmenucode
+									'type'=>'left',			                // This is a Left menu entry
+									'titre'=>'Expédition à préparer',
+									'mainmenu'=>'products',
+									'leftmenu'=>'sendings',
+									'url'=>'/dispatch/liste_expedition_todo.php?leftmenu=sendings',
+									'langs'=>'dispatch@dispatch',	        // Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
+									'position'=>100,
+									'enabled'=>'$conf->dispatch->enabled',  // Define condition to show or hide menu entry. Use '$conf->mymodule->enabled' if entry must be visible if module is enabled. Use '$leftmenu==\'system\'' to show if leftmenu system is selected.
+									'perms'=>'1',			                // Use 'perms'=>'$user->rights->mymodule->level1->level2' if you want your menu with a permission rules
+									'target'=>'',
+									'user'=>2);				                // 0=Menu for internal users, 1=external users, 2=both
+		$r++;
 
 
 		// Exports
