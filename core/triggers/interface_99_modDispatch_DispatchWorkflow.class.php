e1e8ea1d1da5828ec4f055ebf0d202532abdcdb1
maxime peyrot <maxime.peyrot@atm-consulting.fr>
2015-08-06 14:49:58 +0200

    FIX : Evite de créer un doublon dans les mouvements de stock lors de la validation de l'expédition

--- core/triggers/interface_99_modDispatch_DispatchWorkflow.class.php
+++ core/triggers/interface_99_modDispatch_DispatchWorkflow.class.php
@@ -33,11 +33,11 @@
 /**
  *  Class of triggers for Mantis module
  */
- 
+
 class InterfaceDispatchWorkflow
 {
     var $db;
-    
+
     /**
      *   Constructor
      *
@@ -46,15 +46,15 @@ class InterfaceDispatchWorkflow
     function __construct($db)
     {
         $this->db = $db;
-    
+
         $this->name = preg_replace('/^Interface/i','',get_class($this));
         $this->family = "ATM";
         $this->description = "Trigger du module expédtion spécifique Latoxan";
         $this->version = 'dolibarr';            // 'development', 'experimental', 'dolibarr' or version
         $this->picto = 'technic';
     }
-    
-    
+
+
     /**
      *   Return name of trigger file
      *
@@ -64,7 +64,7 @@ class InterfaceDispatchWorkflow
     {
         return $this->name;
     }
-    
+
     /**
      *   Return description of trigger file
      *
@@ -92,7 +92,7 @@ class InterfaceDispatchWorkflow
         else return $langs->trans("Unknown");
     }
 
-	
+
     /**
      *      Function called when a Dolibarrr business event is done.
      *      All functions "run_trigger" are triggered if file is inside directory htdocs/core/triggers
@@ -107,48 +107,50 @@ class InterfaceDispatchWorkflow
 	function run_trigger($action,$object,$user,$langs,$conf)
 	{
 		global $conf,$db;
-		
+
 		if(!defined('INC_FROM_DOLIBARR')) define('INC_FROM_DOLIBARR',true);
-		
+
 		if ($action == 'SHIPPING_VALIDATE') {
 			dol_include_once('/dispatch/config.php');
 			dol_include_once('/dispatch/class/dispatchdetail.class.php');
-			
+
 			$PDOdb = new TPDOdb();
-			
+
 			// Pour chaque ligne de l'expédition
 			foreach($object->lines as $line) {
 				// Chargement de l'objet detail dispatch relié à la ligne d'expédition
 				$dd = new TDispatchDetail();
-				
+
 				$TIdExpeditionDet = TRequeteCore::get_id_from_what_you_want($PDOdb, MAIN_DB_PREFIX.'expeditiondet', array('fk_expedition' => $object->id, 'fk_origin_line' => $line->fk_origin_line));
 				$idExpeditionDet = $TIdExpeditionDet[0];
-				
+
 				//if(!empty($idExpeditionDet) && $dd->loadBy($PDOdb, $idExpeditionDet, 'fk_expeditiondet')) {
 				if(!empty($idExpeditionDet)) {
 					$dd->loadLines($PDOdb, $idExpeditionDet);
-					
+
 					if($conf->asset->enabled){
 						// Création des mouvements de stock de flacon
 						foreach($dd->lines as $detail) {
 							// Création du mouvement de stock standard
+
 							$poids_destocke = $this->create_flacon_stock_mouvement($PDOdb, $detail, $object->ref);
+
 							//$this->create_standard_stock_mouvement($line, $poids_destocke, $object->ref);
-							
+
 							if($conf->clinomadic->enabled){
 								$asset = new TAsset;
 								$asset->load($PDOdb, $detail->fk_asset);
-	
+
 								$prod = new Product($db);
 								$prod->fetch($asset->fk_product);
-								
+
 								//Affectation du type d'équipement pour avoir accès aux extrafields équipement
 								$asset->fk_asset_type = $asset->get_asset_type($PDOdb, $prod->id);
 								$asset->load_asset_type($PDOdb);
-								
+
 								//Localisation client
 								$asset->fk_societe_localisation = $object->socid;
-								
+
 								if(!empty($object->linkedObjects['commande'][0]->array_options['options_duree_pret'])){
 									$asset->etat = 2; //Prêté
 									$asset->set_date('date_deb_pret', $object->date_valid);
@@ -159,11 +161,11 @@ class InterfaceDispatchWorkflow
 								}
 
 								foreach($object->linkedObjects['commande'][0]->lines as $line){
-									if($line->fk_product == $asset->fk_product){									
+									if($line->fk_product == $asset->fk_product){
 										$extension_garantie = $line->array_options['options_extension_garantie'];
 									}
 								}
-								
+
 								$nb_year_garantie+=$prod->array_options['options_duree_garantie_client'];
 
 								$asset->date_fin_garantie_cli = strtotime('+'.$nb_year_garantie.'year', $object->date_valid);
@@ -178,22 +180,22 @@ class InterfaceDispatchWorkflow
 					$this->create_standard_stock_mouvement($line, $line->qty, $object->ref);
 				}*/
 			}
-			
+
 			dol_syslog("Trigger '".$this->name."' for action '$action' launched by ".__FILE__.". id=".$object->id);
 		}
 
 		return 0;
 	}
-	
+
 	private function create_flacon_stock_mouvement(&$PDOdb, &$linedetail, $numref) {
 		global $user, $langs, $conf;
 		dol_include_once('/asset/class/asset.class.php');
 		dol_include_once('/product/class/product.class.php');
 		dol_include_once('/expedition/class/expedition.class.php');
-		
+
 		$asset = new TAsset;
 		$asset->load($PDOdb,$linedetail->fk_asset);
-		
+
 		if($conf->global->clilatoxan){
 			$poids_destocke = $this->calcule_poids_destocke($PDOdb,$linedetail);
 			$poids_destocke = $poids_destocke * pow(10,$asset->contenancereel_units);
@@ -203,13 +205,13 @@ class InterfaceDispatchWorkflow
 		}
 		/*pre($linedetail,true);
 		echo $poids_destocke;exit;*/
-		
+
 		$asset->contenancereel_value = $asset->contenancereel_value - $poids_destocke;
-		$asset->save($PDOdb, $user, $langs->trans("ShipmentValidatedInDolibarr",$numref));
-		
+		$asset->save($PDOdb, $user, $langs->trans("ShipmentValidatedInDolibarr",$numref), 0, false, 0, true);
+
 		return $poids_destocke;
 	}
-	
+
 	/*private function create_standard_stock_mouvement(&$line, $qty, $numref) {
 		global $user, $langs;
 		require_once DOL_DOCUMENT_ROOT.'/product/stock/class/mouvementstock.class.php';
@@ -220,16 +222,16 @@ class InterfaceDispatchWorkflow
 		$result=$mouvS->livraison($user, $line->fk_product, $line->entrepot_id, $qty, $line->subprice, $langs->trans("ShipmentValidatedInDolibarr",$numref));
 		return $result;
 	}*/
-	
+
 	private function calcule_poids_destocke(&$PDOdb,&$linedetail){
-			
+
 		$sql = "SELECT p.weight, p.weight_units
 				FROM ".MAIN_DB_PREFIX."product as p
 					LEFT JOIN ".MAIN_DB_PREFIX."asset as a ON (a.fk_product = p.rowid)
 					LEFT JOIN ".MAIN_DB_PREFIX."expeditiondet_asset as eda ON (eda.fk_asset = a.rowid)
 					LEFT JOIN ".MAIN_DB_PREFIX."expeditiondet as ed ON (ed.rowid = eda.fk_expeditiondet)
 				WHERE ed.rowid = ".$linedetail->fk_expeditiondet;
-		
+
 		$PDOdb->Execute($sql);
 		$PDOdb->Get_line();
 		$weight = $PDOdb->Get_field('weight');
@@ -238,7 +240,7 @@ class InterfaceDispatchWorkflow
 		$poids_unite = (!empty($weight_units)) ? $weight_units : $linedetail->weight_reel_unit ;
 		$poids = $poids * pow(10,$poids_unite);
 		$weight_reel = $linedetail->weight_reel * pow(10,$linedetail->weight_reel_unit );
-		
+
 		return $weight_reel / $poids;
-	} 
+	}
 }
