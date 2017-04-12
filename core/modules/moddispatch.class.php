ed797505df7bc2e2f30d04e271b8bd11daacc5ea
phf <phf@atm-consulting.fr>
2017-04-12 14:35:31 +0200

    Add editor name

--- core/modules/moddispatch.class.php
+++ core/modules/moddispatch.class.php
@@ -43,7 +43,7 @@ class moddispatch extends DolibarrModules
         global $langs,$conf;
 
         $this->db = $db;
-
+		$this->editor_name = 'ATM-Consulting';
 		// Id for module (must be unique).
 		// Use here a free id (See in Home -> System information -> Dolibarr for list of used modules id).
 		$this->numero = 104970;
